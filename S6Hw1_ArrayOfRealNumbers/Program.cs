7dcfa72de5ba52d88d2989db22eb5acf760c4863
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-28 17:38:18 +0300

    Added S6Hw1 task: Print 2d array of real numbers

---
+++ S6Hw1_ArrayOfRealNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+void FillArray(double[,] matrix)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = random.NextDouble() * 10;
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк двухмерного массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите кол-во столбцов двухмерного массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+double[,] matrix = new double[m,n];
+FillArray(matrix);
+PrintArray(matrix);
